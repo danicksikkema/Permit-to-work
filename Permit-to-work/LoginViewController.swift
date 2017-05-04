7ca887483974fad9d2e1852ba6a7f9901ddb3d96
Danick <danick.sikkema@gmail.com>
2017-05-04 12:09:38 +0200

    Added loginviewcontroller

---
+++ Permit-to-work/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Permit-to-work
+//
+//  Created by danick sikkema on 04-05-17.
+//  Copyright © 2017 danick sikkema. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func loginTapped(_ sender: Any) {
+        let mainNavigationController = storyboard?.instantiateViewController(withIdentifier: "MainNavigationController") as! MainNavigationController
+        
+        present(mainNavigationController, animated: true, completion: nil)
+        
+    }
+}
