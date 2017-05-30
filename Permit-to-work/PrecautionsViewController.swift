3bef0184f62624905320bc8cc57dc6f10894884b
Danick <danick.sikkema@gmail.com>
2017-05-30 22:30:54 +0200

    Created precautions page with standard design styling

---
+++ Permit-to-work/PrecautionsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PrecautionsViewController.swift
+//  Permit-to-work
+//
+//  Created by danick sikkema on 30-05-17.
+//  Copyright © 2017 danick sikkema. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PrecautionsViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
