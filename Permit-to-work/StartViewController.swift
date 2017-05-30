5c637ee9f1acf017d118773ec281b0b2922f887b
Danick <danick.sikkema@gmail.com>
2017-05-30 11:13:48 +0200

    Added startview

---
+++ Permit-to-work/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartViewController.swift
+//  Permit-to-work
+//
+//  Created by danick sikkema on 30-05-17.
+//  Copyright © 2017 danick sikkema. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StartViewController : UIViewController {
+    @IBAction func createPermitButton(_ sender: Any) {
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
