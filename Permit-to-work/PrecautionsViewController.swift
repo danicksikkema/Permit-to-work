479dcc1314a02b330a7367ce0d37a2bee76ff126
Danick <danick.sikkema@gmail.com>
2017-06-03 14:09:12 +0200

    Improved findSelected function for precaution page

--- Permit-to-work/PrecautionsViewController.swift
+++ Permit-to-work/PrecautionsViewController.swift
@@ -13,11 +13,12 @@ import Alamofire
 class PrecautionsViewController : UIViewController {
     
     @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
     
     var statusButton1 = true
+    var statusButton2 = true
     
     @IBAction func button1(_ sender: UIButton) {
-        
         switch statusButton1 {
         case false:
             button1.setBackgroundImage(UIImage.init(named: "PrecautionNormal"), for: .normal)
@@ -33,29 +34,46 @@ class PrecautionsViewController : UIViewController {
         }
     }
     
+    @IBAction func button2(_ sender: Any) {
+        switch statusButton2 {
+        case false:
+            button2.setBackgroundImage(UIImage.init(named: "PrecautionNormal"), for: .normal)
+            print ("false")
+            button2.isSelected = false
+            statusButton2 = true
+            
+        case true:
+            button2.setBackgroundImage( UIImage.init(named: "PrecautionActive"), for: .selected)
+            print("true")
+            button2.isSelected = true
+            statusButton2 = false
+        }
+    }
     
     @IBAction func nextButton(_ sender: UIButton) {
         findSelected()
     }
     
     func findSelected () {
-        if button1.isSelected == true {
-            let parameters: [String: String] = ["feedback": (button1.titleLabel?.text!)!]
-            
-            print(parameters)
-            
-            Alamofire.request("http://avhx.com/api/v1/precautions", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseString { response in
+        let buttons: [UIButton] = [button1, button2]
+        
+        for button in buttons {
+            if button.isSelected == true {
+                let parameters: [String: String] = ["feedback": (button.titleLabel?.text!)!]
                 
-                if response.result.value != nil {
-                    debugPrint(response)
-                    print(response.result)
-                    print(response.result.isSuccess)
-                } else {
-                    print("error")
+                Alamofire.request("http://avhx.com/api/v1/precautions", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseString { response in
+                    
+                    if response.result.value != nil {
+                        debugPrint(response)
+                        print(response.result)
+                        print(response.result.isSuccess)
+                    } else {
+                        print("error")
+                    }
                 }
+            } else {
+                button.isSelected = false
             }
-        } else {
-            button1.isSelected = false
         }
     }
     
